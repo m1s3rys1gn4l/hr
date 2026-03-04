dc04a8ade883ee63cdc389ded513ab762e4928e7
m1s3rys1gn4l <m1s3rys1gn4l@gmail.com>
2026-03-05 02:29:24 +0600

    Fix pagination config - use config file instead of deprecated method

---
+++ config/pagination.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination View
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the view that will be used to render the pagination
+    | links on your pages. By default, it is set to use the Tailwind CSS
+    | framework. However, you may change this to another view of your choice.
+    |
+    | Supported: "tailwind", "bootstrap-4", "bootstrap-5", "simple-bootstrap-4",
+    |            "simple-bootstrap-5", "simple-tailwind", "simple-default", "simple-custom"
+    |
+    */
+
+    'default' => 'simple-custom',
+
+];
